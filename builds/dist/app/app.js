d09aa90687116cb1951338b7ac6dba079daa92f3
Vlad <coollife@i.ua>
2018-03-20 22:31:46 +0200

    deploy the environment

---
+++ builds/dist/app/app.js
@@ -0,0 +1,12 @@
+// initialize material design js
+$.material.init();
+
+angular.module('ngFit', []);
+
+/**
+ * Created by szaharov on 28/05/15.
+ */
+
+/**
+ * Created by szaharov on 28/05/15.
+ */

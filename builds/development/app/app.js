7f69fa449aeabc826850c3247a0dd682f83bf862
Vlad <coollife@i.ua>
2018-05-10 22:25:06 +0300

    first commit

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -17,8 +17,10 @@ $.material.init();
                 templateUrl: "/views/about.html"
             }).when('/contacts', {
                 templateUrl: "/views/contacts.html"
-            }).when('/general', {
-                templateUrl: "/views/general.html"
+            }).when('/child1', {
+                templateUrl: "/views/child1.html"
+            }).when('/child2', {
+                templateUrl: "/views/child2.html"
             });
     }
 }());

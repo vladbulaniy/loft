81dec398e9b89f9cc5190946bc8f5f61e9da548c
Vlad <coollife@i.ua>
2018-05-10 22:19:53 +0300

    changes at 10.05.2018

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -1,4 +1,23 @@
 // initialize material design js
 $.material.init();
 
-angular.module('ngFit', []);
+(function () {
+    'use strict';
+    angular
+        .module('ngFit', ['ngRoute'])
+        .config(ngFitConfig);
+
+    ngFitConfig.$inject = ['$routeProvider'];
+
+    function ngFitConfig($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: "/views/index.html"
+            }).when('/about', {
+                templateUrl: "/views/about.html"
+            }).when('/contacts', {
+                templateUrl: "/views/contacts.html"
+            });
+    }
+}());
+

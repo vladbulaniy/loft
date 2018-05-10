96c76953584e094281328fcc7a64139aa4908549
Vlad <coollife@i.ua>
2018-05-10 22:24:01 +0300

    add general state

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -17,6 +17,8 @@ $.material.init();
                 templateUrl: "/views/about.html"
             }).when('/contacts', {
                 templateUrl: "/views/contacts.html"
+            }).when('/general', {
+                templateUrl: "/views/general.html"
             });
     }
 }());
